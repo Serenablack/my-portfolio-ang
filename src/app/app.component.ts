ec2048de11b3af2cf0396712c9a1e54792c75a71
Serenablack <sunsetsin1.sy@gmail.com>
2024-09-20 23:45:39 -0400

    chore: lines removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -60,7 +60,6 @@ export class AppComponent {
       default:
         return;
     }
-    console.log(element, element.nativeElement);
     if (element && element.nativeElement) {
       element.nativeElement.scrollIntoView({
         behavior: 'smooth',
